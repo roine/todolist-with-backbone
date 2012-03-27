41ebb37442a33de21210b86cab2a173c0a4057cc
Jonathan de Montalembert <feunetre@hotmail.com>
2012-03-27 22:37:31 +0800

    added some UI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
  To do List with Backbone
 ========
+
--- js/todo.js
+++ js/todo.js
@@ -15,7 +15,7 @@ $(function(){
 			return 'pas cool';
 		},
 		initialize:function(){
-			console.log('new todo created');
+			console.log('New todo created!');
 			this.bind('error', function(error){
 				console.log(error)
 			});
@@ -24,27 +24,68 @@ $(function(){
 
 	window.TodoCollection = Backbone.Collection.extend({
 		model: Todo,
-		localStorage : new Store("ppl07780"),
+		localStorage : new Store("pml"),
 		initialize:function(){
 			console.log('TodoCollection loaded');
 		}
 
 	});
 
+	window.todoView = Backbone.View.extend({
+		el:$('#todoBox'),
+		initiliaze:function(){
+			this.template = _.template('#todoTemplate').html();
+			_.bindAll(this, 'render');
+			this.model.bind('change', this.render);
+		},
+		setModel:function(model){
+			this.model = model;
+			return this;
+		},
+		render:function(){
+			var renderedContent = this.template(this.model.toJSON());
+			$(this.el).html(renderedContent);
+			return this;
+		}
+	});
+
 	var task = new Array();
-	collection  =  new TodoCollection();
-	var collectionLoaded = collection.fetch();
+	var collection  =  new TodoCollection();
+	collection.fetch();
 	var id = collection.length > 0 ? collection.length+1 : 1;
 	if(collection.isEmpty())
-	console.log('empty collection')
+	console.log('Empty Collection');
+	else{
+
+
+	}
+
 
 	$('#newTodo').click(function(){
 		task[id] = new Todo({id:id});
 		if(collection.add(task[id]))
 		console.log('added in collection');
 		var taskSaved = task[id].save();
-		if(!taskSaved)
+		if(!task[id].save)
 		console.log('Fail saving');
+		else
+		console.log('Successfully saved!')
 		id++;
 	});
+
+	$('#todoBox #title').click(function(){
+		var position = -2;
+		var offset = $('#modifyInputBox').offset();
+		console.log(offset.top+', '+position)
+		if(offset.top === position){
+			$('#modifyInputBox').animate({'top':'-100px'})
+		}else if(offset.top !== position){
+			$('#modifyInputBox').animate({'top':'-2px'})
+			$('#modifyInput').val($(this).text()).focus().select();
+
+		}
+	});
+
+	$('#save').click(function(){
+	})
 });
